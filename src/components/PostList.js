fda6dadf00051fd71edab57a04b6debea138dd22
asanishima <asanishima@gmail.com>
2019-12-04 14:25:34 +0900

    convert components from class to function

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,35 +1,17 @@
 import React from 'react';
 import Post from './Post';
 
-class PostList extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+const PostList = props => {
 
-    render() {
-
-        const postList = [
-            {
-                post: 'hogehogehogehoge',
-                author: 'naoki'
-            },
-            {
-                post: 'fugafugafugafuga',
-                author: 'hogetarou'
-            },
-        ];
-        return (
-            <div className='postlist-container'>
-                <div className=''>
-                    {postList.map( (postItem) => {
-                        return <Post post={postItem.post} author={postItem.author} />
-                    } )}
-                </div>
+    return (
+        <div className='postlist-container'>
+            <div className=''>
+                {props.list.map( (item, index) => {
+                    return <Post key={index} post={item.post} author={item.author} />
+                } )}
             </div>
-
-        )
-    }
-
+        </div>
+    );
 }
 
 export default PostList;
